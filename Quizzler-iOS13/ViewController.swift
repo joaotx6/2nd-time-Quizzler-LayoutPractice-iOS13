01456d71db8ebb67424d5e79f47ead6b293cf609
Joao Teixeira <92153307+joaotx6@users.noreply.github.com>
2022-12-13 10:02:35 +0000

    added error controlling for index range when user answers

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,13 +15,56 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
+    //Array 2D
+    let quiz = [
+        ["5 + 3 its equal to 8", "True"],
+        ["9 + 2 its equal to 7", "False"],
+        ["3 + 1 its equal to 4",  "True"]
+    ]
+    
+    //criaçao de variavel pra acompanhar a questao ou posiçao no array
+    var questionNumber = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
         
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
         
+        //Controlar a resposta do user
+        let userAnswer = sender.currentTitle
+        
+        //A 1a camada corresponde ao valor do questionNumber; E a 2a camada é sempre posição 1, k é onde reside a resposta true ou false
+        let actualAnswer = quiz[questionNumber][1]
+        
+        if userAnswer == actualAnswer {
+            print("Correct!")
+        } else {
+            print("Wrong!")
+        }
+        
+        
+        //controlar o erro, como so tem 3 posiçoes no []
+        if questionNumber < 2 {
+            questionNumber += 1
+        } else {
+            print("Out of index, resetting")
+            questionNumber = 0
+        }
+        
+        ///Nao avançar se estivermos no limite do array.
+//        if questionNumber == 2 {
+//        questionNumber = questionNumber + 0
+//    } else {
+//        questionNumber += 1
+//    }
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber][0]
         
     }
     
