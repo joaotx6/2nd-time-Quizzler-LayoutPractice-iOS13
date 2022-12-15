37e66a379337cf3ba8e346c3bc96b53628216f07
Joao Teixeira <92153307+joaotx6@users.noreply.github.com>
2022-12-15 16:36:06 +0000

    added structure of question

--- Quizzler-iOS13/ViewController.swift
+++ Quizzler-iOS13/ViewController.swift
@@ -15,12 +15,19 @@ class ViewController: UIViewController {
     @IBOutlet weak var trueButton: UIButton!
     @IBOutlet weak var falseButton: UIButton!
     
-    //Array 2D
+    //Evitamos a dificuldade de interpretaçao de um array 2D, assim usamos um
+    //objeto (question) com texto e resposta; Ficando mais facil de interpretar.
     let quiz = [
-        ["5 + 3 its equal to 8", "True"],
-        ["9 + 2 its equal to 7", "False"],
-        ["3 + 1 its equal to 4",  "True"]
+        Question(q: "5 + 3 its equal to 8", a: "True"),
+        Question(q: "9 + 2 its equal to 7", a: "False"),
+        Question(q: "3 + 1 its equal to 4", a: "True")
     ]
+    /* exemplo array 2D (array dentro de um array)
+     let quiz = [
+        ["four + five its equal to seven", "False"]
+     ]
+     */
+    
     
     //criaçao de variavel pra acompanhar a questao ou posiçao no array
     var questionNumber = 0
@@ -28,7 +35,6 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         updateUI()
-        
     }
     
     @IBAction func buttonPressed(_ sender: UIButton) {
@@ -37,7 +43,7 @@ class ViewController: UIViewController {
         let userAnswer = sender.currentTitle
         
         //A 1a camada corresponde ao valor do questionNumber; E a 2a camada é sempre posição 1, k é onde reside a resposta true ou false
-        let actualAnswer = quiz[questionNumber][1]
+        let actualAnswer = quiz[questionNumber].answer
         
         if userAnswer == actualAnswer {
             print("Correct!")
@@ -64,9 +70,7 @@ class ViewController: UIViewController {
     }
     
     func updateUI() {
-        questionLabel.text = quiz[questionNumber][0]
+        questionLabel.text = quiz[questionNumber].text
         
     }
-    
 }
-
